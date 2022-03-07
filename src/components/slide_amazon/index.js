b4ba29b63cb0a24539562832d196d22e4e87bfe5
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-03-07 17:46:39 -0300

    Adicionados containers de promoções e destaques

--- src/components/slide_amazon/index.js
+++ src/components/slide_amazon/index.js
@@ -7,6 +7,14 @@ import banner3 from '../../assets/img/slide_amazon/3.png';
 import banner4 from '../../assets/img/slide_amazon/4.jpg';
 import banner5 from '../../assets/img/slide_amazon/5.jpg';
 import banner6 from '../../assets/img/slide_amazon/6.jpg';
+import produto1 from '../../assets/img/slide_amazon/produtos/1.jpg';
+import produto2 from '../../assets/img/slide_amazon/produtos/2.jpg';
+import produto3 from '../../assets/img/slide_amazon/produtos/3.jpg';
+import produto4 from '../../assets/img/slide_amazon/produtos/4.jpg';
+import produto5 from '../../assets/img/slide_amazon/produtos/5.jpg';
+import produto6 from '../../assets/img/slide_amazon/produtos/6.jpg';
+import produto7 from '../../assets/img/slide_amazon/produtos/7.jpg';
+import ofertas from '../../assets/img/slide_amazon/produtos/foto_ofertas.jpg';
 
 export default function SlideAmazon(){
     const [ banners, setBanners ] = useState([
@@ -58,10 +66,10 @@ export default function SlideAmazon(){
         <section className='container-topo-amazon'>
             <div className='slide-amazon'>
                 <div className='btn-slide btn-slide--anterior' ref={btnAnterior} onClick={voltarSlide}>
-                    <MdOutlineArrowBackIos/>
+                    <MdOutlineArrowBackIos className='btn-slide--icone-anterior'/>
                 </div>
                 <div className='btn-slide btn-slide--proximo' ref={btnProximo} onClick={avancarSlide}>
-                    <MdOutlineArrowForwardIos/>
+                    <MdOutlineArrowForwardIos className='btn-slide--icone-proximo'/>
                 </div>
                 <div className='area-slide' ref={areaSlide}>
                     {
@@ -72,10 +80,50 @@ export default function SlideAmazon(){
                 </div>
             </div>
             <div className='destaques'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div className='destaques--container'>
+                    <h2 className='destaques--titulo'>Informática</h2>
+                    <div className='destaques--box-produtos'>
+                        <div className='produto'>
+                            <img src={produto1} alt='Mousepad da Multilaser'/>
+                        </div>
+                        <div className='produto'>
+                            <img src={produto2} alt='Aparelhos eletrônicos'/>
+                        </div>
+                    </div>
+                </div>
+                <div className='destaques--container'>
+                    <h2 className='destaques--titulo'>Produtos Amazon</h2>
+                    <div className='destaques--box-produtos'>
+                        <div className='produto'>
+                            <img src={produto3} alt='Echo Dot'/>
+                        </div>
+                        <div className='produto'>
+                            <img src={produto4} alt='Kidle'/>
+                        </div>
+                        <div className='produto'>
+                            <img src={produto5} alt='Echo Dot'/>
+                        </div>
+                        <div className='produto'>
+                            <img src={produto6} alt='Fire Stick TV'/>
+                        </div>
+                    </div>
+                </div>
+                <div className='destaques--container'>
+                    <h2 className='destaques--titulo'>Ofertas em eletrônicos</h2>
+                    <div className='destaques--box-anuncio'>
+                        <div className='anuncio'>
+                            <img src={produto7} alt='Ofertas em eletrônicos'/>
+                        </div>
+                    </div>
+                </div>
+                <div className='destaques--container'>
+                    <h2 className='destaques--titulo'>Ofertas em até 70% off</h2>
+                    <div className='destaques--box-anuncio'>
+                        <div className='anuncio'>
+                            <img src={ofertas} alt='Ofertas'/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
