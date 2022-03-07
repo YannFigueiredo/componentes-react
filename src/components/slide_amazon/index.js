9ab135358fa7f1ed2b0788ca7214bfa1801ad909
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-03-07 15:37:03 -0300

    Adicionada funcionalidade de alternar slides

--- src/components/slide_amazon/index.js
+++ src/components/slide_amazon/index.js
@@ -1,5 +1,6 @@
 import './slide_amazon.css';
-import { useState } from 'react';
+import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from 'react-icons/md'
+import { useState, useRef } from 'react';
 import banner1 from '../../assets/img/slide_amazon/1.jpg';
 import banner2 from '../../assets/img/slide_amazon/2.jpg';
 import banner3 from '../../assets/img/slide_amazon/3.png';
@@ -17,10 +18,52 @@ export default function SlideAmazon(){
         {nome: 'Banner de slide 6', url: banner6}
     ]);
 
+    let deslocamento = 0;
+
+    const btnAnterior = useRef(null);
+    const btnProximo = useRef(null);
+    const areaSlide = useRef(null);
+
+    const voltarSlide = (e) => {
+        e.preventDefault();
+
+        btnAnterior.current.classList.add('btn-slide--destacado');
+        btnProximo.current.classList.remove('btn-slide--destacado');
+
+        if(deslocamento + areaSlide.current.clientWidth > 0){
+            deslocamento = ((banners.length - 1) * areaSlide.current.clientWidth) * -1;
+        }else{
+            deslocamento = deslocamento + areaSlide.current.clientWidth;
+        }     
+
+        areaSlide.current.style.marginLeft = deslocamento + 'px';
+    };
+    
+    const avancarSlide = (e) => {
+        e.preventDefault();
+
+        btnProximo.current.classList.add('btn-slide--destacado');
+        btnAnterior.current.classList.remove('btn-slide--destacado');
+
+        if(deslocamento - areaSlide.current.clientWidth === (banners.length * areaSlide.current.clientWidth)*-1){
+            deslocamento = 0;
+        }else{
+            deslocamento = deslocamento - areaSlide.current.clientWidth;
+        }
+
+        areaSlide.current.style.marginLeft = deslocamento + 'px';
+    };
+
     return(
         <section className='container-topo-amazon'>
             <div className='slide-amazon'>
-                <div className='area-slide'>
+                <div className='btn-slide btn-slide--anterior' ref={btnAnterior} onClick={voltarSlide}>
+                    <MdOutlineArrowBackIos/>
+                </div>
+                <div className='btn-slide btn-slide--proximo' ref={btnProximo} onClick={avancarSlide}>
+                    <MdOutlineArrowForwardIos/>
+                </div>
+                <div className='area-slide' ref={areaSlide}>
                     {
                         banners.map((banner, key) => (
                             <img src={banner.url} alt={banner.nome} key={key}/>
